5f768094630c74a7753dc619c8d89c076460acde
xabertum <mohatar@gmail.com>
2018-07-27 14:01:00 +0200

    initial commit

---
+++ src/lecture62/CodigoEstatico.java
@@ -0,0 +1,44 @@
+package lecture62;
+
+public class CodigoEstatico {
+
+    public static void main(String[] args) {
+
+	Persona persona1 = new Persona("Juan");
+	System.out.println("Persona1: " + persona1.toString());
+	
+	//imprimimos el contador de objetos Persona
+	System.out.println("No. Personas " + Persona.getContadorPersonas());
+
+    }
+
+}
+
+class Persona {
+    
+    private String nombre;
+    private int idPersona;
+    private static int contadorPersonas;
+    
+    public Persona(String nombre) {
+	
+	//cada vez que creamos un objeto persona
+	//incrementamos el contador para tener un nuevo idPersona
+	contadorPersonas++;
+	
+	//asignamos el nuevo valor al idPersona
+	idPersona = contadorPersonas;
+	
+	//asignamos el nombre recibido
+	this.nombre = nombre;
+    }
+    
+    public static int getContadorPersonas() {
+	return contadorPersonas;
+    }
+    
+    public String toString() {
+	return "Nombre: " + nombre;
+    }
+    
+}
