d173e4190efa21d70aaf26c9bf22f0615a1d7ccb
xabertum <mohatar@gmail.com>
2018-07-29 12:50:47 +0200

    added example

---
+++ src/lecture64_herencia/Lecture64.java
@@ -0,0 +1,31 @@
+package lecture64_herencia;
+
+
+class Persona {
+    
+    String nombre;
+    
+    public Persona (String nombre) {
+	this.nombre = nombre;
+    }
+}
+
+
+
+public class Lecture64 extends Persona {
+
+    double sueldo;
+    
+    public Lecture64 (String nombre, double sueldo) {
+	super(nombre);
+	this.sueldo = sueldo;
+	
+    }
+    
+    
+    public static void main(String[] args) {
+	Lecture64 lecture64 = new Lecture64("juan", 18000);
+
+    }
+
+}
