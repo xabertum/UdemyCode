f68e6aea1c412e2d9fb94a3742df1c849730ca59
xabertum <mohatar@gmail.com>
2018-07-29 22:03:32 +0200

    Added example

--- src/lecture70_sobrecarga/EjemploSobrecarga.java
+++ src/lecture70_sobrecarga/EjemploSobrecarga.java
@@ -3,7 +3,23 @@ package lecture70_sobrecarga;
 public class EjemploSobrecarga {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	
+	//El constructor vacio es privado, por lo que no obliga
+	//a utilizar el unico constructor publico y por lo tanto
+	//proporcionar valores en los campos nombre y edad
+	
+	Persona persona1 = new Persona("Lilia", 22);
+	System.out.println("Imprimimos el objeto Persona1");
+	System.out.println(persona1);
+	
+	//Creamos un nuevo objeto de tipo persona
+	Persona persona2 = new Persona("Javier", 35);
+	System.out.println("\nImprimimos el objeto Persona2");
+	System.out.println(persona2);
+	
+	//Creamos un objeto empleado
+	Empleado empleado1 = new Empleado("Aroa", 25, 19000);
+	System.out.println(empleado1);
 
     }
 
