d6cc848c71a62f8e1451f7e9ab53b92247f66894
xabertum <mohatar@gmail.com>
2018-07-29 21:57:50 +0200

    added class

--- src/lecture70_sobrecarga/Empleado.java
+++ src/lecture70_sobrecarga/Empleado.java
@@ -6,8 +6,21 @@ public class Empleado extends Persona {
      
      public Empleado (String nombre, int edad, double sueldo) {
 	 super(nombre, edad);
-	 
+	 this.sueldo = sueldo; 
 	 
      }
-    
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+     
+     @Override
+     public String toString () {
+	 return super.toString() + "Empleado { " + "sueldo=" + sueldo + " }";
+     }
+         
 }
