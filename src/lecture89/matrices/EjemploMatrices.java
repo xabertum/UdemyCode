f5eec4976d7b25b0d615f3079a90f94258c08a52
xabertum <mohatar@gmail.com>
2018-08-26 09:44:52 +0200

    added example

--- src/lecture89/matrices/EjemploMatrices.java
+++ src/lecture89/matrices/EjemploMatrices.java
@@ -3,8 +3,30 @@ package lecture89.matrices;
 public class EjemploMatrices {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	
+	// Declaramos e instaciamos una matriz del objeto Persona;
+	
+	Persona personas[][] = new Persona [3][2];
+	
+	// Inicializamos los valores de la matriz de personas;
+	
+	personas[0][0] = new Persona("Juan");
+	personas[0][1] = new Persona("Armando");
+	personas[1][0] = new Persona("Karla");
+	personas[1][1] = new Persona("Laura");
+	personas[2][0] = new Persona("Pedro");
+	personas[2][1] = new Persona("Javier");
+	
+	// Leemos los valores de cada elemento de la matriz
+	
+	for (int i = 0; i < personas.length; i++) {
+	    for (int j = 0; j < personas[i].length; j++) {
+		System.out.println("matriz personas indice: " + i + "-" + j + ": " + personas[i][j]);
+	    }
+	}
 
+	
+	
     }
 
 }
