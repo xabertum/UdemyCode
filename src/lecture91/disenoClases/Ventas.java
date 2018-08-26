7bc8c924182eab2c0cac5d63fc362b4c28e8577f
xabertum <mohatar@gmail.com>
2018-08-26 13:37:15 +0200

    added example

--- src/lecture91/disenoClases/Ventas.java
+++ src/lecture91/disenoClases/Ventas.java
@@ -3,7 +3,22 @@ package lecture91.disenoClases;
 public class Ventas {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	
+	// Creamos varios objetos Producto
+	Producto producto1 = new Producto("camisa", 50.00);
+	Producto producto2 = new Producto("Pantalon", 100.00);
+	Producto producto3 = new Producto("Corbata", 30.00);
+	
+	// Creamos un objeto Orden
+	Orden orden1 = new Orden();
+	
+	// Agregamos los productos a la orden
+	orden1.agregarProducto(producto1);
+	orden1.agregarProducto(producto2);
+	orden1.agregarProducto(producto3);
+	
+	// imprimimos la orden
+	orden1.mostrarOrden();
 
     }
 
