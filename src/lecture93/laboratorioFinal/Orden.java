f39496bdaa93dff7e24512f14087c0379be8eec1
xabertum <mohatar@gmail.com>
2018-08-26 20:29:44 +0200

    Added class

--- src/lecture93/laboratorioFinal/Orden.java
+++ src/lecture93/laboratorioFinal/Orden.java
@@ -2,4 +2,27 @@ package lecture93.laboratorioFinal;
 
 public class Orden {
 
+    private final int idOrden;
+    private final Computadora computadoras [];
+    private static int contadorOrdenes;
+    private int contadorComputadoras;
+    private final static int MAXCOMPUTADORAS = 10;
+    
+    public Orden() {
+	this.idOrden = ++contadorOrdenes;
+	computadoras = new Computadora[MAXCOMPUTADORAS];
+    }
+    
+    public void agregarComputadora (Computadora computadora) {
+	if (contadorComputadoras < MAXCOMPUTADORAS) {
+	    computadoras[contadorComputadoras++] = computadora;
+	} else {
+	    System.out.println("Se ha superado el máximo de computadoras " + MAXCOMPUTADORAS);
+	    
+	}
+    }
+    
+    
+    
+    
 }
