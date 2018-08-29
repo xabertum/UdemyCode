591cfd9f51a072b9264e45cea1b185d59cfd2925
xabertum <mohatar@gmail.com>
2018-08-29 11:33:09 +0200

    added example

--- SegundoCurso/lecture103/argumentosVariables/EjemploVarArgs.java
+++ SegundoCurso/lecture103/argumentosVariables/EjemploVarArgs.java
@@ -3,7 +3,17 @@ package lecture103.argumentosVariables;
 public class EjemploVarArgs {
 
     public static void main(String[] args) {
+	// Imprimimos varios numeros
+	imprimirNumeros(15,20,3,61,75,18,10);
 	
+	System.out.println("");
+	imprimirNumerosElemento(15,20,3,61,75,18,10);
+	
+	System.out.println("");
+	imprimirNumerosForEach(15,20,3,61,75,18,10);
+	
+	System.out.println("");
+	variosParametros("Juan", true, 15,20,14);
 
    }
 
@@ -11,6 +21,15 @@ public class EjemploVarArgs {
 	// Recorremos cada elemento
 	for (int i = 0; i < numeros.length; i++) {
 	    System.out.println("Elemento " + i + numeros[i]);
+	}
+    }
+    
+    public static void imprimirNumerosElemento(int... numeros) {
+	// Recorremos cada elemento
+	int elemento;
+	for (int i = 0; i < numeros.length; i++) {
+	    elemento = numeros[i];
+	    System.out.println("Elemento " + i + elemento);
 	}
     }
     
@@ -22,7 +41,13 @@ public class EjemploVarArgs {
     }
     
     public static void variosParametros (String nombre, boolean valido, int... numeros) {
+	System.out.println("Nombre " + nombre);
+	System.out.println("Valido " + valido);
 	
+	// Usamos un forEach
+	for (int numero : numeros) {
+	    System.out.println("El numero es: " + numero);
+	}
     }
     
 }
