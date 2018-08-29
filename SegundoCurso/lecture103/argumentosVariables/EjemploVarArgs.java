736001338421f566cc128c1eacaa256953d61240
xabertum <mohatar@gmail.com>
2018-08-29 11:25:58 +0200

    added example

--- SegundoCurso/lecture103/argumentosVariables/EjemploVarArgs.java
+++ SegundoCurso/lecture103/argumentosVariables/EjemploVarArgs.java
@@ -5,7 +5,7 @@ public class EjemploVarArgs {
     public static void main(String[] args) {
 	
 
-    }
+   }
 
     public static void imprimirNumeros(int... numeros) {
 	// Recorremos cada elemento
