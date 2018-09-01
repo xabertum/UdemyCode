61bdba4dc8601a1944e30f09b9c754d09276900f
xabertum <mohatar@gmail.com>
2018-09-01 20:44:33 +0200

    Added example

--- src/SegundoCurso/lecture106/bloquesAnonimos/Persona.java
+++ src/SegundoCurso/lecture106/bloquesAnonimos/Persona.java
@@ -2,4 +2,25 @@ package SegundoCurso.lecture106.bloquesAnonimos;
 
 public class Persona {
 
+    private final int idPersona;
+    private static int contadorPersonas;
+
+    // Bloque de inicialización estático
+    static {
+        // Iniciamos el idPersona desde el valor 10
+        contadorPersonas = 10;
+        // Aquí no se pueden usar variables no estaticas
+        // idPersona = 1; (ERROR)
+        System.out.println("Ejecuta bloque estático");
+    }
+
+    // Bloque de codigo para inicializar atributos de la clase
+    // El bloque se copia a cada constructor de la clase
+    {
+        System.out.println("Ejecuta bloque incializador");
+        idPersona = ++contadorPersonas;
+    }
+
+
+
 }
