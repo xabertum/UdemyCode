004b6ea2e5514fe9a53190fc997e8c8d70ba04b6
xabertum <mohatar@gmail.com>
2018-09-04 12:39:24 +0200

    Added example

--- src/SegundoCurso/lecture112/modificadoresAccesoPaq1/Clase2.java
+++ src/SegundoCurso/lecture112/modificadoresAccesoPaq1/Clase2.java
@@ -2,8 +2,30 @@ package SegundoCurso.lecture112.modificadoresAccesoPaq1;
 
 public class Clase2 {
 
-    // Prueba de constructores
+    public Clase2() {
+        // Prueba de constructores
+        System.out.println();
+        //Constructor publico
+        new Clase1(1);
+        //Contructor Protected
+        new Clase1(1,2);
+        // Constructor default o package
+        new Clase1(1,2,3);
+        // Constructor Private
+        //new Clase1(1,2,3,4);
+        System.out.println("Constructor private: Acceso denegado");
+
+    }
 
 
+    public void pruebasDesdeClase2 () {
+        // Acceso a clase 1 desde clase 2
+        Clase1 clase1 = new Clase1();
+        System.out.println();
+
+
+
+
+    }
 
 }
