3aa0a564683c7e3162951740038de3e684b51410
xabertum <mohatar@gmail.com>
2018-09-04 10:48:11 +0200

    Added example

---
+++ src/SegundoCurso/lecture112/modificadoresAcceso/Clase1.java
@@ -0,0 +1,30 @@
+package SegundoCurso.lecture112.modificadoresAcceso;
+
+public class Clase1 {
+
+    // Definición de atributos
+    public int atrPublico = 5;
+    protected int atrProtegido = 6;
+    int atrPâquete = 7;
+    private int atrPrivado;
+
+    // Constructores
+    public Clase1 () {}
+
+    public Clase1(int i) {
+        System.out.println("Constructor Publico 1");
+    }
+
+    protected Clase1 (int i, int j) {
+        System.out.println("Constructor protected 2");
+    }
+
+    Clase1 (int i, int j, int k) {
+        System.out.println("Constructor package 3");
+    }
+
+    private Clase1 (int i, int j, int k, int l) {
+        System.out.println("Constructor privado 4");
+    }
+
+}
