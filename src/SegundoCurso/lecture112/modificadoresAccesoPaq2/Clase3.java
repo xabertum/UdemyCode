37904950d756068f88b2a28c6dd1a83f9e2dfb9b
xabertum <mohatar@gmail.com>
2018-09-05 11:13:46 +0200

    Added example

--- src/SegundoCurso/lecture112/modificadoresAccesoPaq2/Clase3.java
+++ src/SegundoCurso/lecture112/modificadoresAccesoPaq2/Clase3.java
@@ -1,4 +1,13 @@
 package SegundoCurso.lecture112.modificadoresAccesoPaq2;
+import SegundoCurso.lecture112.modificadoresAccesoPaq1.Clase1;
 
-public class Clase3 {
+public class Clase3 extends Clase1 {
+
+    public Clase3() {
+        // Constructor protegido, al ser una subclase puede acceder
+        // aunque esté en otro paquete
+    }
+
+
+
 }
