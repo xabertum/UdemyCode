c11e6a72e53f6378be275e4e66a1fc133fc6097c
xabertum <mohatar@gmail.com>
2018-09-07 13:18:27 +0200

    Added example

--- src/SegundoCurso/lecture117/polimorfismo/EjemploPolimorfismo.java
+++ src/SegundoCurso/lecture117/polimorfismo/EjemploPolimorfismo.java
@@ -4,5 +4,16 @@ public class EjemploPolimorfismo {
 
     public static void main (String [] args) {
 
+        Empleado empleado = new Empleado("Juan", 1000);
+        imprimirDetalles(empleado);
+
+        Gerente gerente = new Gerente("Karla", 2000, "Finanzas");
+    }
+
+    // Observamos que el tipo que recibe el metodo es del tipo padre (Empleado)
+    // Sin embargo al momento de ejecutar el metodo, se ejecuta el del hijo (Gerente)
+    // eso es el polimorfismo, multiples formas pero en tiempo de ejecución
+    private static void imprimirDetalles(Empleado empleado) {
+        System.out.println(empleado.obtenerDetalles());
     }
 }
