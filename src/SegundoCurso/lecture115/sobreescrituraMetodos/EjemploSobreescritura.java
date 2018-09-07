1fa1bff0c6bee2ae483999022b021e0457657d57
xabertum <mohatar@gmail.com>
2018-09-07 10:28:04 +0200

    Added example

--- src/SegundoCurso/lecture115/sobreescrituraMetodos/EjemploSobreescritura.java
+++ src/SegundoCurso/lecture115/sobreescrituraMetodos/EjemploSobreescritura.java
@@ -3,6 +3,11 @@ package SegundoCurso.lecture115.sobreescrituraMetodos;
 public class EjemploSobreescritura {
 
     public static void main (String [] args ) {
+        Empleado empleado = new Empleado("Juan", 1000);
+        System.out.println(empleado.obtenerDetalles());
+
+        Gerente gerente = new Gerente("Karla", 2000, "Finanzas");
+        System.out.println(gerente.obtenerDetalles());
 
     }
 
