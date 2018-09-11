60dc49debe892582e56fb9199631a37d2ffae7ad
xabertum <mohatar@gmail.com>
2018-09-11 10:32:40 +0200

    Added example

--- src/SegundoCurso/lecture119/instanceOf/Circulo.java
+++ src/SegundoCurso/lecture119/instanceOf/Circulo.java
@@ -2,6 +2,7 @@ package SegundoCurso.lecture119.instanceOf;
 
 public class Circulo extends FiguraGeometrica {
 
+    @Override
     public void dibujar() {
         System.out.println("Dibujar Circulo");
     }
