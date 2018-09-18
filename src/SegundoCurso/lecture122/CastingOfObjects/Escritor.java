81d0bd28d93313377c31584b3f193c3fc08f9fb3
xabertum <mohatar@gmail.com>
2018-09-18 10:10:55 +0200

    Added example

---
+++ src/SegundoCurso/lecture122/CastingOfObjects/Escritor.java
@@ -0,0 +1,4 @@
+package SegundoCurso.lecture122.CastingOfObjects;
+
+public class Escritor extends Empleado{
+}
---
+++ src/SegundoCurso/lecture122/CastingOfObjects/Gerente.java
@@ -0,0 +1,4 @@
+package SegundoCurso.lecture122.CastingOfObjects;
+
+public class Gerente extends Empleado {
+}
