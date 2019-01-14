0a848f327ead294481afa79a347ce98bd6930009
xabertum <mohatar@gmail.com>
2019-01-14 19:17:23 +0100

    Added example

--- src/SegundoCurso/lecture129/interfaces/manejointerfaces/ManejoInterfaces.java
+++ src/SegundoCurso/lecture129/interfaces/manejointerfaces/ManejoInterfaces.java
@@ -1,4 +1,26 @@
 package SegundoCurso.lecture129.interfaces.manejointerfaces;
 
+import SegundoCurso.lecture129.interfaces.datos.AccesoDatos;
+import SegundoCurso.lecture129.interfaces.datos.ImplementacionMysql;
+import SegundoCurso.lecture129.interfaces.datos.ImplementacionOracle;
+
 public class ManejoInterfaces {
+
+    public static void main (String [] args) {
+        AccesoDatos datos = new ImplementacionOracle();
+        ejecutar(datos, "listar");
+
+        datos = new ImplementacionMysql();
+        ejecutar (datos, "insertar");
+    }
+
+    private static void ejecutar (AccesoDatos datos, String accion) {
+        if ("listar".equals(accion)) {
+            datos.listar();
+        }
+        else if ("insertar".equals(accion)) {
+            datos.insertar();
+        }
+    }
+
 }
