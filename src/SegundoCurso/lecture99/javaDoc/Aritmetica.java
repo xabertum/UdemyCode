45795c7905a8ed1a117c4c02b8222b19501cf5d1
xabertum <mohatar@gmail.com>
2019-07-16 02:37:51 +0200

    minor format changes

--- src/SegundoCurso/lecture99/javaDoc/Aritmetica.java
+++ src/SegundoCurso/lecture99/javaDoc/Aritmetica.java
@@ -1,6 +1,5 @@
 package SegundoCurso.lecture99.javaDoc;
 
-
 /**
  * Esta clase permite realizar operaciones de arimetica basica.
  * 
@@ -9,31 +8,33 @@ package SegundoCurso.lecture99.javaDoc;
  *
  */
 public class Aritmetica {
-    /**
-     * Primer operando
-     */
-    int operandoA;
-        
-    /**
-     * Segundo operando
-     */
-    int operandoB;
-    
-    /**
-     * Constructor vacio de la clase
-     * @param j 
-     * @param i 
-     */
-    public Aritmetica(int i, int j) {
-	this.operandoA = i;
-	this.operandoB = j;
-    }
-    
-    /**
-     * Este metodo realiza la suma de dos operandos enteros
-     * @return int resultado de la suma
-     */
-    public int sumar() {
-	return operandoA + operandoB;
-    }
+	/**
+	 * Primer operando
+	 */
+	int operandoA;
+
+	/**
+	 * Segundo operando
+	 */
+	int operandoB;
+
+	/**
+	 * Constructor vacio de la clase
+	 * 
+	 * @param j
+	 * @param i
+	 */
+	public Aritmetica(int i, int j) {
+		this.operandoA = i;
+		this.operandoB = j;
+	}
+
+	/**
+	 * Este metodo realiza la suma de dos operandos enteros
+	 * 
+	 * @return int resultado de la suma
+	 */
+	public int sumar() {
+		return operandoA + operandoB;
+	}
 }
